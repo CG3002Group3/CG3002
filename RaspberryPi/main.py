88df45daf69656b0d2461d709858d9967e3bcb28
Ashley Ong <ashley_2461994@hotmail.com>
2018-03-22 14:41:06 +0800

    change all tabs to spaces

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -10,82 +10,80 @@ import base64
 import testdeserial as tds
 import predict as predict
 
-useServer = 0
+useServer = 1
 collect_test_data = 0
 testing_samples = 1
 
 def readlineCR(port): 
-	rv="" 
-	while True: 
-		ch=port.read() 
-		rv+=ch 
-		if ch=='\r' or ch =='': 
-			return rv
+    rv="" 
+    while True: 
+        ch=port.read() 
+        rv+=ch 
+        if ch=='\r' or ch =='': 
+            return rv
 
 class Data():
-	def __init__(self, socket):
-		self.bs = 32
-		self.secret_key = "1234512345123451"
-		self.voltage = 0 
-		self.current = 0 
-		self.power = 0  #voltage * current
-		self.cumpower=0
-		self.sock = socket
-		self.sample_queue = collections.dequeue([], 20)
+    def __init__(self, socket):
+        self.bs = 32
+        self.secret_key = "1234512345123451"
+        self.voltage = 0 
+        self.current = 0 
+        self.power = 0  #voltage * current
+        self.cumpower=0
+        self.sock = socket
+        self.sample_queue = collections.dequeue([], 20)
 
-	def pad(self, msg):
-		return msg + (self.bs - len(msg)%self.bs)*chr(self.bs - len(msg)%self.bs)
+    def pad(self, msg):
+        return msg + (self.bs - len(msg)%self.bs)*chr(self.bs - len(msg)%self.bs)
 
-	def encryptText(self, msg, secret_key):
-		raw = self.pad(msg)
-		iv = Random.new().read(AES.block_size)
-		cipher = AES.new(secret_key,AES.MODE_CBC,iv)
-		return base64.b64encode(iv + cipher.encrypt(raw))
+    def encryptText(self, msg, secret_key):
+        raw = self.pad(msg)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(secret_key,AES.MODE_CBC,iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
 
-	def sendData(self, predictedMove):
-		formattedAnswer = ("#"+ str(predictedMove) + "|" + str(self.voltage)\
-			+ "|" + str(self.current) + "|" + str(self.power) + "|" + str(self.cumpower) + "|")
-		print(formattedAnswer)
-		encryptedText = self.encryptText(formattedAnswer, self.secret_key)
-		self.sock.send(encryptedText)
+    def sendData(self, predictedMove):
+        formattedAnswer = ("#"+ str(predictedMove) + "|" + str(self.voltage)\
+            + "|" + str(self.current) + "|" + str(self.power) + "|" + str(self.cumpower) + "|")
+        print(formattedAnswer)
+        encryptedText = self.encryptText(formattedAnswer, self.secret_key)
+        self.sock.send(encryptedText)
 
 class RaspberryPi():
-	def __init__(self):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.test_counter = 0
-		self.isHandshakeDone = False
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.test_counter = 0
+        self.isHandshakeDone = False
 
-	def connectToServer(self):
-		IPAddress = sys.argv[1]
-		Port = int(sys.argv[2])
-		server_address = (IPAddress, Port)
-		self.sock.connect(server_address)
+    def connectToServer(self):
+        IPAddress = sys.argv[1]
+        Port = int(sys.argv[2])
+        server_address = (IPAddress, Port)
+        self.sock.connect(server_address)
 
-	def connectToArduino(self):
+    def connectToArduino(self):
         #self.serial_port= serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=3.0) #For linux
-		self.serial_port=serial.Serial("/dev/serial0", baudrate=115200, timeout=0) #For the Rpi
-		print("Port Open!")
-		self.serial_port.reset_input_buffer()
-		self.serial_port.reset_output_buffer()
+        self.serial_port=serial.Serial("/dev/serial0", baudrate=115200, timeout=0) #For the Rpi
+        print("Port Open!")
+        self.serial_port.reset_input_buffer()
+        self.serial_port.reset_output_buffer()
 
-	def run(self):
-		try:
-			#Connections
+    def run(self):
+        try:
+            #Connections
             if(useServer):
                 self.connectToServer()
                 print("Connected to test server")
                 data = Data(self.sock)
 
-			self.connectToArduino()
+            self.connectToArduino()
 
-			#Handshaking with Arduino
-			while(self.isHandshakeDone == False):
+            #Handshaking with Arduino
+            while(self.isHandshakeDone == False):
                 self.serial_port.write('H')
                 print("H sent")
                 time.sleep(0.5)
-                #print(self.serial_port.in_waiting)
                 reply = self.serial_port.read(1)
-                #print(reply)
                 if(reply == 'B'):
                     self.isHandshakeDone = True
                     self.serial_port.write('F')
@@ -95,56 +93,57 @@ class RaspberryPi():
                 else:
                     time.sleep(0.5)
                     
-			#if no byte to read
-			#if(self.serial_port.in_waiting != 0 or self.serial_port.read() ):
-			#	print("Disconnected")
+            #if no byte to read
+            #if(self.serial_port.in_waiting != 0 or self.serial_port.read() ):
+            #   print("Disconnected")
 
-			#Receive data from Arduino(periodically) and save to CSV
-			send_flag = True
-			while(collect_test_data):
+            #Receive data from Arduino(periodically) and save to CSV
+            send_flag = True
+            while(collect_test_data):
                 #instruction = raw_input("Type the next command")
                 if (send_flag == True):
                     self.serial_port.write('R')
                     send_flag = False
                 time.sleep(0.1)
-			    if(self.serial_port.inWaiting() > 0):
+                if(self.serial_port.inWaiting() > 0):
                     arduino_data = readlineCR(self.serial_port)
                     print(arduino_data)
                     tds.save_data(arduino_data)
                     send_flag = True
                     tds.calc_checksum(arduino_data)
-			
-			#Read and Predict the results
-			while(testing_samples):
+            
+            #Read and Predict the results
+            while(testing_samples):
                 if (send_flag == True):
                     self.serial_port.write('R')
                     send_flag = False
                 time.sleep(0.1)
-			    if(self.serial_port.inWaiting() > 0):
+                if(self.serial_port.inWaiting() > 0):
+                    send_flag = True
                     arduino_data = readlineCR(self.serial_port)
                     data.sample_queue.appendleft(arduino_data.split("\n")[:-1])
                     if(len(data.sample_queue) == 20):
                         predict.predict_data(list(data.sample_queue))
                                 
-			# deserialise_data = tds.get_data(arduino_data)
-			# print(deserialise_data)
-			# self.test_counter += 1
-			# line_to_send = "A" + str(self.test_counter)
-			# print(line_to_send)
-			# self.serial_port.write(line_to_send)
-					
-			#test communication with server.
-##			while(True):
-##				name = raw_input("What is the dance move?")
-##				data.voltage = 12
-##				data.current = 1
-##				data.power = 100
-##				data.cumpower
-##				data.sendData(name)
+            # deserialise_data = tds.get_data(arduino_data)
+            # print(deserialise_data)
+            # self.test_counter += 1
+            # line_to_send = "A" + str(self.test_counter)
+            # print(line_to_send)
+            # self.serial_port.write(line_to_send)
+                    
+            #test communication with server.
+##          while(True):
+##              name = raw_input("What is the dance move?")
+##              data.voltage = 12
+##              data.current = 1
+##              data.power = 100
+##              data.cumpower
+##              data.sendData(name)
 
-		except KeyboardInterrupt:
-			sys.exit(1)
+        except KeyboardInterrupt:
+            sys.exit(1)
 
 if __name__ == '__main__':
-	pi = RaspberryPi()
-	pi.run()
+    pi = RaspberryPi()
+    pi.run()
